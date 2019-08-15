50a1093b12765119a958311b27c031a3eeb8a3ed
gustavopompeu <gustavopompeu_@hotmail.com>
2019-08-15 11:05:40 -0300

    15-08

--- codigo.R
+++ codigo.R
@@ -20,3 +20,19 @@ summary(obitos)
 # Ocorrências Aeronáuticas na Aviação Civil Brasileira
 oco <- read.csv("oco.csv", sep="~", encoding="UTF-8")
 summary(oco)
+
+## José Carlos (Ministério da Economia) e Carlos Antonio (Secretaria de comércio exterior)
+# Base de dados do Comex Stat
+#Base de dados detalhada por NCM: Arquivos CSV com separador ponto e vírgula (;) detalhado por ano, mês, 
+#código NCM, código da unidade estatística, código de país de destino/origem do produto, 
+#código da UF de origem/destino do produto, código da via de transporte, código da URF de embarque/desembarque, 
+#quantidade estatística, quilograma líquido, valor dólar FOB (US$).
+ncm <- read.table("EXP_2019.txt", sep=";", header=T)
+summary(ncm)
+
+#Base de dados detalhada por Município da empresa exportadora/importadora e Posição do Sistema Harmonizado (SH4): 
+#Arquivos CSV com separador ponto e vírgula (;) detalhado por ano, mês, código SH4, 
+#código de país de destino/origem do produto, código da UF do domicíilo fiscal da empresa, 
+#código do município domicílio fiscal da empresa exportadora/importadora, quilograma líquido, valor dólar FOB (US$).
+imp <- read.table("IMP_2019_MUN.txt", sep=";", header=T)
+summary(imp)
